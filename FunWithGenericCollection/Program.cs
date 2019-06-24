0d2650d688593bc069d7c515e5b489e8ae5d216e
Alexandr <tolstov.alexandr.85@gmail.com>
2019-06-24 08:49:46 +0600

    1

--- FunWithGenericCollection/Program.cs
+++ FunWithGenericCollection/Program.cs
@@ -13,7 +13,8 @@ namespace FunWithGenericCollection
             //UseGenericList();
             //UseGenericStack();
             //UseGenericQueue();
-            UseSortedSet();
+            //seSortedSet();
+            UseDictionary();
         }
         static void UseGenericList()
         {
@@ -106,6 +107,33 @@ namespace FunWithGenericCollection
             {
                 Console.WriteLine(p);
             }
+        }
+        static void UseDictionary()
+        {
+            Dictionary<string, Person> peopleA = new Dictionary<string, Person>();
+            peopleA.Add("Homer", new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            peopleA.Add("Marge", new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            peopleA.Add("Lisa", new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+            Person homer = peopleA["Homer"];
+            Console.WriteLine(homer);
+
+            Dictionary<string, Person> peopleB = new Dictionary<string, Person>()
+            {
+                { "Homer", new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 } },
+                { "Marge", new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 } },
+                { "Lisa", new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 } }
+            };
+            Person lisa = peopleB["Lisa"];
+            Console.WriteLine(lisa);
+
+            Dictionary<string, Person> peopleC = new Dictionary<string, Person>()
+            {
+                ["Homer"] = new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 },
+                ["Marge"] = new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 },
+                ["Lisa"] = new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 }
+            };
+            Person marge = peopleC["Marge"];
+            Console.WriteLine(marge);
         }
     }
 }
